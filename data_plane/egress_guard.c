df6621d48ca2f2ad0b0bc9e418088ca8cbb4c642
josiah <josiahd.kitchin@gmail.com>
2026-02-24 12:27:18 -0800

    changed ingress guard to do all of the filtering

--- data_plane/egress_guard.c
+++ data_plane/egress_guard.c
@@ -2,15 +2,6 @@
 
 #include "guards.h"
 
-struct
-{
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __type(key, char[MAX_QNAME_LEN]); // Domain Name
-    __type(value, uint8_t);           // is blocked
-    __uint(max_entries, MAX_BLOCKED_LENGTH);
-    __uint(pinning, LIBBPF_PIN_BY_NAME);
-
-} blkd_domain_map SEC(".maps");
 
 /* Buffer to write DNS queries for further inspection in user space */
 struct
@@ -26,21 +17,23 @@ struct query_event
 };
 
 /* Tunnel guard for outgoing traffic (egress) from the DNS resolver
+ * 
+ * Finds the associated IP address with the query and removes the mapping 
  *
- * Inspects DNS queries being sent from the resolver to other DNS servers so that only uncached
- * queries are checked
+ * If the query is outgoing to another DNS server, the query and client
+ * IP address is pushed to a shared ringbuffer with the control plane and allowed to pass. 
  *
- * Drops the DNS query if the qname is blocked
- *
- * Only handles IPv4 at this time
- *
- * Does not block based on IP address, that is left to the incoming traffic guard (ingress)
- *
+ * The egress guard should not be relied to filer queries, as that is left
+ * to the ingress guard. Queries should be passed through the egress guard, 
+ * only being blocked after the control plane as determined a query suspicious, updating 
+ * the blocked maps the ingress guard uses. 
  */
 
 SEC("cgroup_skb/egress")
 int tunnel_guard_egress(struct __sk_buff* skb)
 {
+
+
     void* data_end = (void*)(long)skb->data_end;
     void* data     = (void*)(long)skb->data;
 
@@ -58,7 +51,9 @@ int tunnel_guard_egress(struct __sk_buff* skb)
 
     /* ---------------------------- Transport ---------------------------- */
 
+
     uint16_t dst_port;
+    uint16_t src_port; 
     void*    dns_header;
     void*    transport_header = (void*)ip_header + ip_header->ihl * 4;
 
@@ -67,7 +62,8 @@ int tunnel_guard_egress(struct __sk_buff* skb)
         struct udphdr* udp_header = transport_header;
         if ((void*)udp_header + sizeof(struct udphdr) >= data_end)
             return PASS;
-        dst_port   = udp_header->dest;
+        dst_port   = bpf_ntohs(udp_header->dest);
+        src_port   = bpf_ntohs(udp_header->source); 
         dns_header = (void*)udp_header + sizeof(struct udphdr);
     }
     else if (ip_header->protocol == IPPROTO_TCP)
@@ -88,9 +84,8 @@ int tunnel_guard_egress(struct __sk_buff* skb)
      * This is egress traffic, we only need to analyze queries going to DNS servers,
      * not traffic being sent back to the client
      */
-
-    if (bpf_ntohs(dst_port) != DNS_PORT)
-        return PASS;
+    if (dst_port != DNS_PORT)
+        return PASS; 
 
     char* qname = dns_header + DNS_HDR_LEN;
 
@@ -117,88 +112,29 @@ int tunnel_guard_egress(struct __sk_buff* skb)
 
     uint32_t* ip_addr_p = bpf_map_lookup_elem(&query_to_ip, full_qname);
 
+    /* There should be a mapping when query is leaving resolver to DNS server. */ 
+
     if (!ip_addr_p)
-        return DROP;
+        return DROP; 
+
 
     uint32_t ip_addr = bpf_htonl(*ip_addr_p);
-
     bpf_map_delete_elem(&query_to_ip, full_qname);
 
-    /*
-     * Check each subdomain and drop if it is blocked
-     * EX: JFDSL.attacker.com
-     * Checks JFDSL.attacker.com
-     * Then checks attacker.com
-     * Then .com
-     * Checks in wire format, not presentation
-     */
+    struct query_event* event = bpf_ringbuf_reserve(&query_events, sizeof(struct query_event), 0); 
 
-    int remaining_label_chars = 0;
-    for (int i = 0; i < len; i++)
+    if (!event)
+        return DROP; 
+
+    event->ip_address = bpf_ntohl(ip_addr); 
+    for (int i = 0; i < MAX_QUERY_LEN; i++)
     {
-        if (remaining_label_chars == 0)
-        {
-            char sub_domain[MAX_QNAME_LEN] = {0};
+        if (dns_header + i >= data_end)
+            break;
 
-            int copy_len = len - i + 1;
-
-            if (bpf_probe_read_kernel(sub_domain, copy_len, qname + i) < 0)
-                return DROP;
-
-            if (bpf_map_lookup_elem(&blkd_domain_map, sub_domain))
-                return DROP;
-
-            remaining_label_chars = qname[i];
-        }
-        else
-        {
-            remaining_label_chars--;
-        }
+        event->query_data[i] = ((char*)dns_header)[i];
     }
-
-    /* Filter by QTYPE */
-
-    uint16_t* qtype_ptr = (uint16_t*)(qname + len + 1);
-
-    if ((char*)(qtype_ptr + 1) > (char*)data_end)
-        return DROP;
-
-    uint16_t qtype = bpf_ntohs(*qtype_ptr);
-
-    /* Drop queries of unnallowed query types */
-    switch (qtype)
-    {
-    case QTYPE_A:
-    case QTYPE_AAAA:
-    case QTYPE_CNAME:
-    case QTYPE_MX:
-    case QTYPE_NS:
-    case QTYPE_SOA:
-    case QTYPE_PTR:
-    {
-        /* Pass query to userspace for further inspection */ 
-
-        struct query_event* event = bpf_ringbuf_reserve(&query_events, sizeof(struct query_event), 0); 
-
-        if (!event)
-            return DROP; 
-
-        event->ip_address = bpf_ntohl(ip_addr); 
-        for (int i = 0; i < MAX_QUERY_LEN; i++)
-        {
-            if (dns_header + i >= data_end)
-                break;
-
-            event->query_data[i] = ((char*)dns_header)[i];
-        }
-        bpf_ringbuf_submit(event, 0);
-
-        return PASS;
-    }
-
-    default:
-        return DROP;
-    }
+    bpf_ringbuf_submit(event, 0);
 
     return PASS;
 }
